bfb99c19b0ade7c3fa420a8e35d5622c97c60769
Welson Gunawan <welsonoktario@gmail.com>
2024-10-14 18:14:46 +0700

    revert: add public path setting

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,11 +5,6 @@ use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
-    ->registered(function($app) {
-        if (env('APP_ENV') === 'production') {
-            $app->usePublicPath(__DIR__.env('APP_PUBLIC_PATH', '/../../../public_html/fleetnl.technivine.com'));
-        }
-    })
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         api: __DIR__.'/../routes/api.php',
