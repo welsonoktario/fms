f19665e72c95594ebb446a79c9695d1f32631e9f
firman herda <firmanherda26@gmail.com>
2024-10-17 11:02:37 +0700

    update index drivers

--- resources/views/units/qr.blade.php
+++ resources/views/units/qr.blade.php
@@ -5,6 +5,6 @@
   </div>
   <div class="mt-4 rounded-lg p-4 shadow bg-base-100">
     <a href="{{ route('units.show', $units->asset_code) }}">Detail Unit</a>
-    <a href="{{ route('$', $units->asset_code) }}">TESTING</a>
+    <a href="{{ route('#', $units->asset_code) }}">TESTING</a>
   </div>
 </x-layouts.app>
