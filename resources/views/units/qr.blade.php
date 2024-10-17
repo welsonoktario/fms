65f10cf361db7149465642281da0aa4527469f17
firman herda <firmanherda26@gmail.com>
2024-10-17 11:10:32 +0700

    update index drivers

--- resources/views/units/qr.blade.php
+++ resources/views/units/qr.blade.php
@@ -3,10 +3,10 @@
     <h1 class="text-xl font-semibold">Unit Details</h1>
     <a href="{{ route('units.index') }}" class="btn btn-sm btn-secondary">Back to List</a>
   </div>
- <div class="mt-4 rounded-lg p-4 shadow bg-base-100">
+  <div class="mt-4 rounded-lg p-4 shadow bg-base-100">
     <h3 class="text-lg font-semibold mb-4">Aksi:</h3>
-    <div class="flex space-x-4">
-        <a href="{{ route('units.show', $units->asset_code) }}" class="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition">Detail Unit</a>
+    <div class="flex flex-col space-y-2">
+        <a href="{{ url('units/detail/' . $units->asset_code) }}" class="bg-blue-500 text-black px-4 py-2 rounded hover:bg-blue-600 transition">Detail Unit</a>
         <br>
         <a href="https://www.facebook.com" class="bg-gray-500 text-black px-4 py-2 rounded hover:bg-gray-600 transition" target="_blank" rel="noopener noreferrer">Akun Sosmed Fleet NL</a>
     </div>
