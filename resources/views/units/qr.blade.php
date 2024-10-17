e195df7bb08c446812554e9994bfaad212c77d11
firman herda <firmanherda26@gmail.com>
2024-10-17 11:07:04 +0700

    update index drivers

--- resources/views/units/qr.blade.php
+++ resources/views/units/qr.blade.php
@@ -5,9 +5,9 @@
   </div>
   <div class="mt-4 rounded-lg p-4 shadow bg-base-100">
     <h3 class="text-lg font-semibold mb-4">Aksi:</h3>
-    <div class="flex space-x-4">
+    <div class="flex flex-col space-y-2">
         <a href="{{ route('units.show', $units->asset_code) }}" class="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition">Detail Unit</a>
-        <a href="https://www.facebook.com" class="bg-gray-500 text-white px-4 py-2 rounded hover:bg-gray-600 transition" target="_blank" rel="noopener noreferrer">Akun Sosmed Fleet NL</a>
+        <a href="https://www.facebook.com" class="text-blue-500 hover:underline" target="_blank" rel="noopener noreferrer">Akun Sosmed Fleet NL</a>
     </div>
 </div>
 </x-layouts.app>
