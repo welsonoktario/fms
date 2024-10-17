8ce5d0e7928a32b35ad64d0f9f3dc65ace8e8f4d
firman herda <firmanherda26@gmail.com>
2024-10-17 11:05:47 +0700

    update index drivers

--- resources/views/units/qr.blade.php
+++ resources/views/units/qr.blade.php
@@ -4,7 +4,10 @@
     <a href="{{ route('units.index') }}" class="btn btn-sm btn-secondary">Back to List</a>
   </div>
   <div class="mt-4 rounded-lg p-4 shadow bg-base-100">
-    <a href="{{ route('units.show', $units->asset_code) }}">Detail Unit</a>
-    <a href="#">TESTING</a>
-  </div>
+    <h3 class="text-lg font-semibold mb-4">Aksi:</h3>
+    <div class="flex space-x-4">
+        <a href="{{ route('units.show', $units->asset_code) }}" class="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition">Detail Unit</a>
+        <a href="https://www.facebook.com" class="bg-gray-500 text-white px-4 py-2 rounded hover:bg-gray-600 transition" target="_blank" rel="noopener noreferrer">Akun Sosmed Fleet NL</a>
+    </div>
+</div>
 </x-layouts.app>
