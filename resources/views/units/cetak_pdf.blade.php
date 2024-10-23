3ff603635b01a6ec30706d544bd65075cb6732c3
firman herda <firmanherda26@gmail.com>
2024-10-23 09:38:16 +0700

    update daily monitoring

--- resources/views/units/cetak_pdf.blade.php
+++ resources/views/units/cetak_pdf.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Barcode PDF - {{ $units->asset_code }}</title>
+    <title>Barcode PDF - {{ isset($units[0]) ? $units[0]->asset_code : '' }}</title> <!-- Menampilkan asset_code pertama dalam collection -->
     <style>
         body {
             font-family: Arial, sans-serif;
@@ -19,13 +19,15 @@
     </style>
 </head>
 <body>
-    <h1>Barcode for Asset Code: {{ $units->asset_code }}</h1>
+    <h1>Barcode for Asset Code: {{ isset($units[0]) ? $units[0]->asset_code : '' }}</h1> <!-- Menampilkan asset_code pertama -->
     <div class="barcode">
-        @if($units->image_barcode)
-            <img src="{{ Storage::url('img/qrunits/' . $units->image_barcode) }}" alt="Barcode Image">
-        @else
-            <p>No Barcode Image Available</p>
-        @endif
+        @foreach ($units as $u)
+            @if($u->image_barcode)
+                <img src="{{ Storage::url($u->image_barcode) }}" alt="QR Code" width="100" height="100">
+            @else
+                <p>No Barcode Image Available for {{ $u->asset_code }}</p>
+            @endif
+        @endforeach
     </div>
 </body>
 </html>
