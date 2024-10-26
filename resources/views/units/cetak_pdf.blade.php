ea26e51d5a77da719bc4ad0703131684840f8a0a
Welson Gunawan <welsonoktario@gmail.com>
2024-10-26 22:16:00 +0700

    fix cetak pdf view

--- resources/views/units/cetak_pdf.blade.php
+++ resources/views/units/cetak_pdf.blade.php
@@ -11,35 +11,50 @@
     }
 
     .barcode {
-      margin: 30px;
+      margin-left: auto;
+      margin-right: auto;
       text-align: center;
+      width: 500px;
     }
 
     img {
       width: 200px;
       height: auto;
     }
-    h1 {
-  margin-top: 10px; /* Memberikan sedikit jarak antara gambar dan teks */
-}
+
+    table {
+      width: 250px;
+      margin-left: auto;
+      margin-right: auto;
+    }
   </style>
 </head>
 
 <body>
-
-  <div class="barcode">
-    @if ($units->image_barcode)
-      <img src="data:image/svg;base64,{{ base64_encode(file_get_contents(public_path($units->image_barcode))) }}"
-        alt="QR Code" width="200" height="200">
-    @else
-      <p>No Barcode Image Available</p>
-    @endif
-    {{-- <img src="{{ asset('storage/img/qrunits/testing.jpg') }}" alt="Testing"> --}}
-    <img class="image" src="{{ public_path() . '/img/qrunits/logoakas.jpg' }}">
-    {{-- <img src="data:image/jpg;base64,{{ base64_encode(file_get_contents(public_path('/img/qrunits/logoakas.jpg'))) }}" --}}
-        alt="QR Code" width="200" height="200">
-    <h1> {{ $units->asset_code }}</h1>
-  </div>
+  <table>
+    <tr>
+      <td style="text-align: left;">
+        <img
+          src="data:image/jpg;base64,{{ base64_encode(file_get_contents(public_path('/storage/img/qrunits/logoakas.jpg'))) }}"
+          alt="QR Code" style="width: 64px; height: 64px;">
+      </td>
+      <td style="text-align: right;">
+        <h1>{{ $units->asset_code }}</h1>
+      </td>
+    </tr>
+    <tr>
+      <td style="text-align: center;
+          padding-top: 1rem;" colspan="2">
+        @if ($units->image_barcode)
+          {{-- <img src="/{{ $units->image_barcode }}" alt="QR Code" style="width: 100%; height: 100%"> --}}
+          <img src="data:image/svg;base64,{{ base64_encode(file_get_contents(public_path($units->image_barcode))) }}"
+            alt="QR Code" style="width: 250px; height: 250px">
+        @else
+          <p>No Barcode Image Available</p>
+        @endif
+      </td>
+    </tr>
+  </table>
 </body>
 
 </html>
