6565574d1376dad3704360fbb899485cf756040f
firman herda <firmanherda26@gmail.com>
2024-10-26 12:11:04 +0700

    update daily monitoring

--- resources/views/units/cetak_pdf.blade.php
+++ resources/views/units/cetak_pdf.blade.php
@@ -34,8 +34,7 @@
     @else
       <p>No Barcode Image Available</p>
     @endif
-    <img src="data:image/jpg;base64,{{ base64_encode(file_get_contents(public_path('storage/img/qrunits/testing.jpg'))) }}"
-    alt="Testing Image" width="50" height="50">
+    <img src="{{ asset('storage/img/qrunits/testing.jpg') }}" alt="Testing">
     <h1> {{ $units->asset_code }}</h1>
   </div>
 </body>
