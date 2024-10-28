f4fccc0fc8d47d273b1b920e4083c0ad214ad85f
firman herda <firmanherda26@gmail.com>
2024-10-28 10:28:35 +0700

    update daily monitoring

--- app/Http/Controllers/UnitController.php
+++ app/Http/Controllers/UnitController.php
@@ -54,7 +54,7 @@ class UnitController extends Controller
             'name' => 'required|string|max:255',
             'meter' => 'required|string|max:255',
             'colour' => 'required|string|max:255',
-            'type' => 'required|string|in:DUMPTRUCK',
+            'type' => 'required|string|in:DUMPTRUCK,HEAVY EQUIPMENT,OPERATIONAL CAR',
             'chassis_number' => 'required|string|max:255',
             'engine_number' => 'required|string|max:255',
             'engine_model' => 'required|string|max:255',
--- resources/views/units/create.blade.php
+++ resources/views/units/create.blade.php
@@ -109,6 +109,8 @@
             class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
             <option value="" disabled selected>Choose Type</option>
             <option value="DUMPTRUCK" {{ old('type') == 'DUMPTRUCK' ? 'selected' : '' }}>DUMPTRUCK</option>
+            <option value="HEAVY EQUIPMENT" {{ old('type') == 'HEAVY EQUIPMENT' ? 'selected' : '' }}>HEAVY EQUIPMENT</option>
+            <option value="OPERATIONAL CAR" {{ old('type') == 'OPERATIONAL CAR' ? 'selected' : '' }}>OPERATIONAL CAR</option>
           </select>
           @error('type')
             <div class="text-red-600 text-sm mt-1">{{ $message }}</div>
